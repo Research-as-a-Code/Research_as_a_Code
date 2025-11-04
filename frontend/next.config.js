76873a4c55e979204af1bdbec47312f9d505417e
MrCsabaToth <csaba.toth.us@gmail.com>
2025-11-04 09:33:59 -0800

    Containerizing the frontend

---
+++ frontend/next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:8000',
+  },
+  output: 'standalone', // For Docker deployment
+}
+
+module.exports = nextConfig
+
